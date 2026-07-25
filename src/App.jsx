5c20af90d2c3e8ebcbd6703a0e8795197459db02
ajitmit321@gmail.com <ajitmit321@gmail.com>
2026-07-25 16:06:29 +0530

    update sonarqube display meassage

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1>Github Actions</h1>
-      <h2>Complete course with 2 projects</h2>
+      <h5>Complete course with sonar-scan integration</h5>
     </>
   )
 }
